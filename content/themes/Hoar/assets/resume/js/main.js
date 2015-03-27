45a9f5539d44b5707bbf84ef0aaad7edf6998bea
liu <liu@4d6e2614-77ed-46c3-a56a-120efffb6f41>
2015-03-27 15:06:08 +0000

    前后端消息校验

--- content/themes/Hoar/assets/resume/js/main.js
+++ content/themes/Hoar/assets/resume/js/main.js
@@ -160,76 +160,93 @@ jQuery(document).ready(function($) {
     /* -------------------------- Contact Form ------------------------------ */
     /* ---------------------------------------------------------------------- */
 
+    function checkmessage(msg){
+        var msg_error = msg.split(",");
+        var output_error = '';
+
+        if (msg_error.indexOf('error-message') != -1) {
+            $("#contact-message").addClass("has-error");
+            $("#contact-message").removeClass("has-success");
+            output_error = 'Please enter your message.';
+        } else {
+            $("#contact-message").addClass("has-success");
+            $("#contact-message").removeClass("has-error");
+        }
+
+        if (msg_error.indexOf('error-email') != -1) {
+
+            $("#contact-email").addClass("has-error");
+            $("#contact-email").removeClass("has-success");
+            output_error = 'Please enter valid e-mail.';
+        } else {
+            $("#contact-email").addClass("has-success");
+            $("#contact-email").removeClass("has-error");
+        }
+
+        if (msg_error.indexOf('error-name') != -1) {
+            $("#contact-name").addClass("has-error");
+            $("#contact-name").removeClass("has-success");
+            output_error = 'Please enter your name.';
+        } else {
+            $("#contact-name").addClass("has-success");
+            $("#contact-name").removeClass("has-error");
+        }
+
+        if (msg == 'success') {
+
+            response = '<div class="alert alert-success success-send">' +
+                    '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+                    '<i class="glyphicon glyphicon-ok" style="margin-right: 5px;"></i> ' + $success
+                    + '</div>';
+
+            $(".reset").trigger('click');
+            $("#contact-name").removeClass("has-success");
+            $("#contact-email").removeClass("has-success");
+            $("#contact-message").removeClass("has-success");
+
+        } else {
+
+            response = '<div class="alert alert-danger error-send">' +
+                    '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+                    '<i class="glyphicon glyphicon-remove" style="margin-right: 5px;"></i> ' + output_error
+                    + '</div>';
+
+        }
+        // Hide any previous response text
+        $(".error-send,.success-send").remove();
+        // Show response message
+        $contactform.prepend(response);
+    }
+
     // Needed variables
     var $contactform = $('#contactform');
     var $success = ' Your message has been sent. Thank you!';
 	var response = '';
 
     $('#contactform').submit(function() {
+        var name = $("#contact-name").find("input").val();
+        var email = $("#contact-email").find("input").val();
+        var message = $("#contact-message").find("textarea").val();
+        var errors = [];
+        if(name === ""){
+            errors.push("error-name");
+        }
+        if(message === ""){
+            errors.push("error-name");
+        }
+        var expr = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+        if(expr.test(email) == false){
+            errors.push("error-email");
+        }
+        if(errors.length != 0){
+            checkmessage(errors.join(","));
+            return false;
+        }
         $.ajax({
             type: "POST",
             url: "/message",
             data: $(this).serialize(),
-            success: function(msg)
-            {
-                var msg_error = msg.split(",");
-                var output_error = '';
-
-                if (msg_error.indexOf('error-message') != -1) {
-                    $("#contact-message").addClass("has-error");
-                    $("#contact-message").removeClass("has-success");
-                    output_error = 'Please enter your message.';
-                } else {
-                    $("#contact-message").addClass("has-success");
-                    $("#contact-message").removeClass("has-error");
-                }
-
-                if (msg_error.indexOf('error-email') != -1) {
-
-                    $("#contact-email").addClass("has-error");
-                    $("#contact-email").removeClass("has-success");
-                    output_error = 'Please enter valid e-mail.';
-                } else {
-                    $("#contact-email").addClass("has-success");
-                    $("#contact-email").removeClass("has-error");
-                }
-
-                if (msg_error.indexOf('error-name') != -1) {
-                    $("#contact-name").addClass("has-error");
-                    $("#contact-name").removeClass("has-success");
-                    output_error = 'Please enter your name.';
-                } else {
-                    $("#contact-name").addClass("has-success");
-                    $("#contact-name").removeClass("has-error");
-                }
-
-
-
-                if (msg == 'success') {
-
-                    response = '<div class="alert alert-success success-send">' +
-                            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
-                            '<i class="glyphicon glyphicon-ok" style="margin-right: 5px;"></i> ' + $success
-                            + '</div>';
-
-                    $(".reset").trigger('click');
-                    $("#contact-name").removeClass("has-success");
-                    $("#contact-email").removeClass("has-success");
-                    $("#contact-message").removeClass("has-success");
-
-                } else {
-
-                    response = '<div class="alert alert-danger error-send">' +
-                            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
-                            '<i class="glyphicon glyphicon-remove" style="margin-right: 5px;"></i> ' + output_error
-                            + '</div>';
-
-                }
-                // Hide any previous response text
-                $(".error-send,.success-send").remove();
-                // Show response message
-                $contactform.prepend(response);
-            }
+            success: checkmessage
         });
         return false;
     });
